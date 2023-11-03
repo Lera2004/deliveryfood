c77cbf04909ffaf6dbfc589fcde74f3e5069441e
Lera Neroznak <76168462+Lera2004@users.noreply.github.com>
2023-11-03 10:27:19 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,105 @@
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+    modal.classList.toggle("is-open");
+}
+
+
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+let login = localStorage.getItem('gloDelivery');
+
+function toggleModalAuth() {
+    modalAuth.classList.toggle('is-open');
+    if (modalAuth.classList.contains('is-open')) {
+        disableScroll();
+    } else {
+        enableScroll();
+    }
+}
+
+function clearForm() {
+    loginInput.style.borderColor = '';
+    logInForm.reset();
+}
+
+function authorized() {
+    function logOut() {
+        login = null;
+        localStorage.removeItem('gloDelivery');
+        userName.style.display = 'none';
+        buttonOut.style.display = 'none';
+        buttonAuth.style.display = '';
+        toggleModalAuth();
+        buttonOut.removeEventListener('click', logOut);
+        checkAuth();
+    }
+
+    console.log('Авторизован');
+    userName.textContent = login;
+    buttonAuth.style.display = 'none';
+    userName.style.display = 'inline';
+    buttonOut.style.display = 'block';
+
+    buttonOut.addEventListener('click', logOut);
+}
+
+function notAuthorized() {
+    console.log('Не авторизован');
+
+    function logIn(event) {
+        event.preventDefault();
+        
+
+        if (loginInput.value.trim()) {
+            login = loginInput.value;
+            localStorage.setItem('gloDelivery', login);
+            toggleModalAuth();
+            buttonAuth.style.display = 'none';
+            userName.style.display = 'inline';
+            buttonOut.style.display = 'block';
+            buttonAuth.removeEventListener('click', toggleModalAuth);
+            closeAuth.removeEventListener('click', toggleModalAuth);
+            logInForm.removeEventListener('submit', logIn);
+            logInForm.reset();
+            checkAuth();
+        } else {
+            loginInput.style.borderColor = '#ff0000';
+            loginInput.value = '';
+        }
+    }
+
+    buttonAuth.addEventListener('click', toggleModalAuth);
+    closeAuth.addEventListener('click', toggleModalAuth);
+    logInForm.addEventListener('submit', logIn);
+    modalAuth.addEventListener('click', function (event) {
+        console.log(event.target);
+        if (event.target.classList.contains('is-open')) {
+            toggleModalAuth()
+        }
+    })
+}
+
+buttonAuth.addEventListener('click', clearForm);
+
+function checkAuth() {
+    if (login) {
+        authorized();
+    } else {
+        notAuthorized();
+    }
+}
+
+checkAuth();
