59bc193d03cb481fb597b45510970513cbaba84b
Lera Neroznak <76168462+Lera2004@users.noreply.github.com>
2023-11-10 12:29:56 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
+'use strist';
+
 const cartButton = document.querySelector('#cart-button');
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
-
-function toggleModal() {
-    modal.classList.toggle("is-open");
-}
-
-
-
 const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
@@ -18,8 +10,19 @@ const logInForm = document.querySelector('#logInForm');
 const loginInput = document.querySelector('#login');
 const userName = document.querySelector('.user-name');
 const buttonOut = document.querySelector('.button-out');
+const cardsRestaurants = document.querySelector('.cards');
+const containerPromo = document.querySelector('.container-promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
 
 let login = localStorage.getItem('gloDelivery');
+
+function toggleModal() {
+    modal.classList.toggle("is-open");
+}
+
 
 function toggleModalAuth() {
     modalAuth.classList.toggle('is-open');
@@ -103,3 +106,95 @@ function checkAuth() {
 }
 
 checkAuth();
+
+function createCardsRestaurant() {
+    const card = `
+        <a  class="card">
+            <img src="img/pizza1.jpg" alt="image1" class="card-image">
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title">IQ Pizza</h3>
+                        <span class="card-tag tag">20 мин</span>
+                </div>
+                <div class="card-info">
+                    <div class="raiting">
+                        <img src="img/rating.svg" alt="raiting" class="raiting-star">
+                            4.5
+                    </div>
+                    <div class="price">От 69₴</div>
+                    <div class="category">Пицца</div>
+                </div>
+            </div>
+        </a>
+    `;
+
+    cardsRestaurants.insertAdjacentHTML('beforeend', card);
+}
+
+createCardsRestaurant();
+createCardsRestaurant();
+createCardsRestaurant();
+
+function createCardGood() {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    card.insertAdjacentHTML('beforeend', `
+        <img src="img/pizza1.jpg" alt="image1" class="card-image">
+        <div class="card-text">
+            <div class="card-heading">
+                <h3 class="card-title card-title-reg">Ролл угорь стандарт</h3>
+            </div>
+            <div class="card-info">
+                <div class="ingredients">Рис, угорь, соус унаги, кунжут, водоросли нори.</div>
+            </div>
+            <div class="card-buttons">
+                <button class="button button-primary">
+                    <span class="button-card-text">В корзину</span>
+                    <img src="img/shopping-cart2.svg" alt="shopping-card" class="button-card-image">
+                </button>
+                <strong class="card-price-bold">250 ₴</strong>
+            </div>
+       </div>
+    `);
+
+    cardsMenu.insertAdjacentElement('beforeend', card);
+}
+
+function openGoods(event) {
+    
+
+    const target = event.target;
+
+    const restaurant = target.closest('.card');
+
+
+if(login) {
+
+if (restaurant) {
+        cardsMenu.textContent = '';
+        containerPromo.classList.add('hide')
+        restaurants.classList.add('hide')
+        menu.classList.remove('hide')
+
+        createCardGood();
+        createCardGood();
+        createCardGood();
+    }
+	} else {
+		toggleModalAuth();
+	}
+}
+
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+
+
+cardsRestaurants.addEventListener('click', openGoods);
+logo.addEventListener('click', function () {
+    containerPromo.classList.remove('hide')
+        restaurants.classList.remove('hide')
+        menu.classList.add('hide')
+})
